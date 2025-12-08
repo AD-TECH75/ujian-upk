1e71e5100958eb682cfb95d40dfeeaf27ff75a74
AD_TECH75 <muhammadabidhafizalauddin6@gmail.com>
2025-12-08 10:23:23 +0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+function login() {
+    // setting email dan password
+    let email = 'admin@gmail.com';
+    let password = 'admin';
+
+    // mengambil email dan password dari input
+    let formEmail = document.getElementById('email').value;
+    let formPassword = document.getElementById('password').value;
+
+    // mengambil id error
+    let error = document.getElementById('error');
+
+    // verifikasi email dan password
+    if (formEmail == email && formPassword == password) {
+        error.style.display = "none"; // memastikan masih kosong
+        window.location = 'dashboard.html'; // pindah halaman
+        return false; // mencegah reload
+    } else {
+        error.innerHTML = "Email atau Password Salah";
+        error.style.display = "block"; // mernampilkan pesan
+        return false; //mencegah reload
+    }
+}
